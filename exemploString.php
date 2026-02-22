d72f2326aa6184057c55b6b6bb98fe06c96d6e90
amandaspader <amanda.spader@ifpr.edu.br>
2026-02-22 16:54:15 -0300

    Adição do exemplo de String

---
+++ exemploString.php
@@ -0,0 +1,16 @@
+<?php
+
+    echo "<h3>Exemplo de String</h3>";//tag de cabeçalho
+    echo "<h4>-> Sem variável</h4>";
+    echo "Olá, eu sou a Amanda e este é um exemplo de texto em PHP sem variável.";
+    echo "<br>"; //tag de quebra de linha em HTML
+
+    echo "<br>";
+
+    echo "<h4>-> Com variável</h4>";
+    $minhaString = 'Este texto estava em uma String e foi impressa normalmente.';
+    echo "O que está escrito na váriável?";
+    echo "<br>";
+    echo $minhaString;
+
+?>
