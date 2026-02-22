3396b91320957cfe856d360fcc1f5e95f590fe44
amandaspader <amanda.spader@ifpr.edu.br>
2026-02-22 16:51:25 -0300

    Adição do exemplo de números float

---
+++ exemploFloat.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo "<h3>Exemplo de números float</h3>";//tag de cabeçalho
+    echo "<h4>-> Sem variável</h4>";
+    echo 4.4;
+    echo "<br>"; //tag de quebra de linha em HTML
+    echo 5.6;
+
+    echo "<br>";
+
+    echo "<h4>-> Com variável e soma</h4>";
+    $numUm = 5.9;
+    $numDois = 3.5;
+    echo "Número 1: $numUm";
+    echo "<br>";
+    echo "Número 2: $numDois";
+    echo "<br>";
+    echo "Soma dos dois: ";
+    echo $numUm + $numDois;
+
+?>
