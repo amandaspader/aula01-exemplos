6d1317f3835a28e29e4823af381fc1499f4ee325
amandaspader <amanda.spader@ifpr.edu.br>
2026-02-22 16:37:18 -0300

    Exemplos feito em sala

---
+++ primeiraPagina.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "Hello World!";
+    echo "<br>";
+    echo "Linha 2";
+    echo "<br>";
+    echo "<br>";
+    echo "<h4>Teste de número</h4>";
+    $numA = 2;
+    echo "Número $numA";
+    echo "<br>";
+    $numB = 4.4;
+    echo "<br>";
+    echo "Número $numB";
+    $texto = 'Oi, sou um tijolo';
+    echo "<br>";
+    echo "Apresentação: $texto";
+
+?>
